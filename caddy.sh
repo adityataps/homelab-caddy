4a02d8444be6f4369fe68e4e333caec16359271d
Aditya Tapshalkar <aditya.taps@gmail.com>
2024-12-29 15:50:27 -0500

    fix: add watch flag, remove update command

--- caddy.sh
+++ caddy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-set -e
-
 # Start reverse-proxy
-caddy start --config /app/caddy/Caddyfile --envfile /app/caddy/.env || caddy reload --config /app/caddy/Caddyfile --envfile /app/caddy/.env
+caddy start --config /app/caddy/Caddyfile --envfile /app/caddy/.env --watch

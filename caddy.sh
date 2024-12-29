f770d5c65367d54301b8c0d02eefac9c934a9110
Aditya Tapshalkar <aditya.taps@gmail.com>
2024-12-29 15:28:20 -0500

    fix: use .env in /app/caddy

--- caddy.sh
+++ caddy.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Check if .env exists
-if [ ! -f .env ]; then
+if [ ! -f /app/caddy/.env ]; then
    echo ".env file not found!"
    exit 1
 fi
 
 # Export .env variables
-export "$(grep -v '^#' .env | xargs)"
+export "$(grep -v '^#' /app/caddy/.env | xargs)"
 
 # Start reverse-proxy
 caddy start || caddy reload --config /app/caddy/Caddyfile

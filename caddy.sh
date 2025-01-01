7dc81aed64469859128f7866c252e0e05e117619
Aditya Tapshalkar <aditya.taps@gmail.com>
2025-01-01 03:19:57 -0500

    feat: update run/reload caddy script, with symlink and copying root ca certificate to homelab storage

--- caddy.sh
+++ caddy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-# Start reverse-proxy
-caddy start --config /app/caddy/Caddyfile --envfile /app/caddy/.env --watch
+set -e
+
+# Symlink config files
+touch /app/caddy.env
+ln -sf /app/caddy/.env /etc/caddy/.env
+ln -sf /app/caddy/Caddyfile /etc/caddy/Caddyfile
+
+# Copy Caddy root CA self-signed cert to mounted volume
+mkdir -p /mnt/homelab/network/caddy/cert || {
+  echo "Failed to create directory for storing Caddy self-signed cert";
+  exit 1;
+}
+cp /var/lib/caddy/.local/share/caddy/pki/authorities/local/root.crt /mnt/homelab/network/caddy/cert/root.crt
+
+caddy start --envfile /etc/caddy/.env -w || caddy reload
